6abfb85de35c5966dee1faa7392fe8659df3ce71
Ramakrishna-12 <parigaram.ram12@gmail.com>
2020-02-08 22:46:24 +0530

    second commit

---
+++ maven/maven-newyeargift/src/main/java/com/test/maven_newyeargift/App.java
@@ -0,0 +1,23 @@
+package com.test.maven_newyeargift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void sweets1(String sweet)
+	{
+		System.out.println(sweet);
+	}
+	public static void sweet2(String sweet)
+	{
+		System.out.println(sweet);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        sweets1("gulab jamun");
+        sweet2("laddu");        
+    }
+}
